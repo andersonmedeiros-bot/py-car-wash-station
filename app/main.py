126105d084be4b78963b53009fc47a94eeb564fd
Anderson <anderson.medeiros3011@gmail.com>
2026-05-07 18:21:10 -0300

    corrigir: atualizar nomes de variáveis e corrigir comprimento da linha

--- app/main.py
+++ app/main.py
@@ -1,12 +1,7 @@
 class Car:
-    def __init__(
-        self,
-        comfort_level: int,
-        cleanliness_level: int,
-        brand: str
-    ) -> None:
-        self.comfort_level = comfort_level
-        self.cleanliness_level = cleanliness_level
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
         self.brand = brand
 
 
@@ -14,34 +9,37 @@ class CarWashStation:
     def __init__(
         self,
         distance_from_city_center: float,
-        clean_threshold: int,
+        clean_power: int,
         average_rating: float,
         count_of_ratings: int
     ) -> None:
         self.distance_from_city_center = distance_from_city_center
-        self.clean_threshold = clean_threshold
+        self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list[Car]) -> float:
+    def serve_cars(self, cars: list) -> float:
         income = 0.0
         for car in cars:
-            if car.cleanliness_level < self.clean_threshold:
+            if car.clean_mark < self.clean_power:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        price = (car.comfort_level
-                 * (self.clean_threshold - car.cleanliness_level)
-                 * self.average_rating) / self.distance_from_city_center
+        price = (
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+        ) / self.distance_from_city_center
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
-        if self.clean_threshold > car.cleanliness_level:
-            car.cleanliness_level = self.clean_threshold
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rating: int) -> None:
         total_rating_sum = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
-        self.average_rating = round((total_rating_sum + rating) / self.count_of_ratings, 1)
+        new_average = (total_rating_sum + rating) / self.count_of_ratings
+        self.average_rating = round(new_average, 1)
